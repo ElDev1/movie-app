ab90bec6666aaac4e6ac823dca1fa0d836d6af68
Devi Amaolo <deviamaolo@gmail.com>
2022-07-11 02:02:41 -0300

    building form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,16 @@ import React from 'react'
 const Login = () => {
   return (
     <form>
-        <input type='email' name='email' />
-        <input type='password' name='password'/>
+        <label>
+            <span>Email</span> <br/>
+            <input type='email' name='email' /> 
+        </label>
+        <br />
+        <label>
+            <span>Password</span> <br/>
+            <input type='password' name='password'/> 
+        </label>
+        <br />
         <button type='submit'>Login</button>
     </form>
   )
