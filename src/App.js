4d827d90a81913d1a352e11aceddc8bce7b9ac9d
Devi Amaolo <deviamaolo@gmail.com>
2022-07-13 00:33:56 -0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Login from './components/Login'
 import List from './components/List';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './CSS/bootstrap.min.css'
 
 import { Routes, Route } from 'react-router-dom'
 
+
 function App() {
   return (
-    <>
+    <div className='container'>
+      <Header />
       <Routes>
         <Route exact path='/' element={<Login />} />
         <Route path='/list' element={<List />} />
       </Routes>
-    </>
+      <Footer />
+    </div>
   );
 }
 
