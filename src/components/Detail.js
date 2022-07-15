1bf7185cb396e0d27c7d05459715ad63027ea92b
Devi Amaolo <deviamaolo@gmail.com>
2022-07-15 02:35:03 -0300

    creating details card

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,13 @@
 import { Navigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Detail = () => {
   
   const token = sessionStorage.getItem('token') 
   
+  const [movie, setMovie] = useState(null)
+
   const query = new URLSearchParams(window.location.search)
   const movieID = query.get('movieID')
 
@@ -15,10 +17,11 @@ const Detail = () => {
       axios.get(endPoint)
         .then((res) => {
           const apiData = res.data
-          
+          setMovie(apiData)
         })
         .catch(err => {
           console.log(err)
+          alert('status code 404')
         })
 
     },[movieID])
@@ -26,23 +29,27 @@ const Detail = () => {
   return (
     <>
       {!token && <Navigate to='/' />}
-      <h2>Movie details</h2>
-      <div className='row'>
-        <div className='col-4'>
-          imagen
-        </div>
-        <div className='col-8'>
-          <h5>Date</h5>
-          <h5>Review</h5>
-          <p>data about the movie</p>
-          <h5>genre</h5>
-          <ul>
-            <li>action</li>
-            <li>terror</li>
-            <li>terror</li>
-          </ul>
-        </div>
-      </div>
+      {!movie && <p>Loading...</p>}
+      {movie && (
+        <>
+          <h2>{movie.title}</h2>
+          <div className='row'>
+            <div className='col-4'>
+              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className='img-fluid' alt='movie poster'></img>
+            </div>
+            <div className='col-8'>
+              <h5>Release Date: {movie.release_date}</h5>
+              <h5>Overview</h5>
+              <p>{movie.overview}</p>
+              <h5>Rating: {movie.vote_average}</h5>
+              <h5>Genre</h5>
+              <ul>
+                {movie.genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+              </ul>
+            </div>
+          </div>
+        </>
+      )}
     </>
   )
 }
